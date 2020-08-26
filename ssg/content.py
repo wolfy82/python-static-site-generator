31dbad54391d92875087fb70e358bf524a141ac3
Aaron Roth <aarongroth@gmail.com>
2020-08-26 15:24:23 -0700

    interim commit

---
+++ ssg/content.py
@@ -0,0 +1,13 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimiter = r'^(?:-|\+){3}\s*$'
+    __regex = re.compile(__delimiter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = yaml.load(fm, Loader=FullLoader)
+        return cls(metadata, content)
