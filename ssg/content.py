1355c95f06dd5c2b552ef78603ef92f90fac0feb
Aaron Roth <aarongroth@gmail.com>
2020-08-26 16:19:34 -0700

    Module Complete

--- ssg/content.py
+++ ssg/content.py
@@ -11,3 +11,35 @@ class Content(Mapping):
         _, fm, content = cls.__regex.split(string, 2)
         metadata = yaml.load(fm, Loader=FullLoader)
         return cls(metadata, content)
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        return self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data = dict()
+        for key, value in self.data.items():
+            if key != "content":
+                data[key] = value
+        return str(data)
